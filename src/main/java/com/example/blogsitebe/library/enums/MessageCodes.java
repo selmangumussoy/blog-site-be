d4b8c40a8fbb0896138dc534af5b601c10dd2cc3
selmangummussoy <gumussoyhsg@gmail.com>
2025-02-09 00:59:32 +0300

    messagecode access

--- src/main/java/com/example/blogsitebe/library/enums/MessageCodes.java
+++ src/main/java/com/example/blogsitebe/library/enums/MessageCodes.java
@@ -1,4 +1,18 @@
 package com.example.blogsitebe.library.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum MessageCodes {
+    SUCCESS("200", "general.success"),
+    FAIL("500", "general.fail");
+
+    private final String code;
+    private final String message;
+
+    MessageCodes(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
 }
