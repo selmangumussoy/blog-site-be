62b778e6b8e47a99adeb752f8976d2d1e1be43d6
selmangummussoy <gumussoyhsg@gmail.com>
2025-02-18 23:04:07 +0300

    core blog entity

---
+++ src/main/java/com/example/blogsitebe/domain/platform/blog/Blog.java
@@ -0,0 +1,15 @@
+package com.example.blogsitebe.domain.platform.blog;
+
+import com.example.blogsitebe.library.rest.AbstractEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Blog extends AbstractEntity {
+
+}
