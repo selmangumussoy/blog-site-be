a54ba782d846812d4fbca4fbacfad807a473a7c0
selmangummussoy <gumussoyhsg@gmail.com>
2025-02-20 05:57:15 +0300

    crud methods added

--- src/main/java/com/example/blogsitebe/domain/platform/tag/api/TagService.java
+++ src/main/java/com/example/blogsitebe/domain/platform/tag/api/TagService.java
@@ -1,5 +1,16 @@
 package com.example.blogsitebe.domain.platform.tag.api;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface TagService {
     TagDto save(TagDto dto);
+
+    void delete(String id);
+
+    TagDto getById(String id);
+
+    Page<TagDto> getAll(Pageable pageable);
+
+    TagDto update(String id, TagDto dto);
 }
