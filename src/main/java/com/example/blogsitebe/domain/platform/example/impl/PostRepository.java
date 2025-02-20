63419f4e2beb79437f1ffa32d95cced280b96e01
selmangummussoy <gumussoyhsg@gmail.com>
2025-02-20 04:48:27 +0300

    delete unnecessary things

--- src/main/java/com/example/blogsitebe/domain/platform/example/impl/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.blogsitebe.domain.platform.example.impl;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
