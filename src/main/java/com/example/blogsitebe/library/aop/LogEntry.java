19e3d13d6ac8109e2a5028a32a12f7797ff256b3
selmangummussoy <gumussoyhsg@gmail.com>
2025-05-03 17:14:04 +0300

    logentry table name

--- src/main/java/com/example/blogsitebe/library/aop/LogEntry.java
+++ src/main/java/com/example/blogsitebe/library/aop/LogEntry.java
@@ -4,16 +4,18 @@ package com.example.blogsitebe.library.aop;
 import com.example.blogsitebe.library.rest.AbstractEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = LogEntry.TABLE)
 public class LogEntry extends AbstractEntity {
+    public static final String TABLE = "log_entry";
 
     @Column(nullable = false)
     private String httpMethod;
