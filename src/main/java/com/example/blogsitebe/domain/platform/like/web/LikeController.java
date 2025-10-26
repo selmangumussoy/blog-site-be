814facb1fa110c71fb0da70fdd6067304b8f4fd8
selmangummussoy <gumussoyhsg@gmail.com>
2025-10-26 14:37:50 +0300

    refactor

--- src/main/java/com/example/blogsitebe/domain/platform/like/web/LikeController.java
+++ src/main/java/com/example/blogsitebe/domain/platform/like/web/LikeController.java
@@ -4,7 +4,12 @@ import com.example.blogsitebe.domain.platform.like.api.LikeDto;
 import com.example.blogsitebe.library.abstraction.AbstractController;
 import com.example.blogsitebe.library.abstraction.AbstractService;
 import com.example.blogsitebe.library.abstraction.AbstractWebMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping("/api/likes")
 public class LikeController  extends AbstractController<LikeDto, LikeRequest, LikeResponse> {
     public LikeController(AbstractService<LikeDto> service, AbstractWebMapper<LikeDto, LikeRequest, LikeResponse> mapper) {
         super(service, mapper);
